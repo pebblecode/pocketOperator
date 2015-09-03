54eb0601ded6ba407680885f3e73733cc7d7964e
Martin Schinz <m.schinz@gmail.com>
2015-09-03 12:10:15 +0100

    gate start

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+var gate = require('./js/audio_gate');
 var rx = require('rx');
+var $ = require('jquery');
 var eventProducer = rx.Observable.interval(500);
 var callback = function(){console.log('event')};
+$(document).ready(function(){
+  var audioElement = document.querySelector('audio');
+  console.log(audioElement);
+  var on_off_element = $('#btn_on_off');
+  var on_off = rx.Observable.fromEvent(on_off_element, 'click');
 
-eventProducer.subscribe(function(onNext){
-	callback();
-	//console.log('event')
-},undefined, undefined);
+  gate(audioElement, on_off);
+  eventProducer.subscribe(function(onNext, on_off){
+      callback();
+      //console.log('event')
+  },undefined, undefined)
+});
+
